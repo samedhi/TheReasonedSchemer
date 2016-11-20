81f1c57d77082d7f372ce0635e51c72bc5fa678e
Stephen Cagle <stephen@read-line.com>
2016-11-20 11:44:46 -0800

    Complete chapter 1

--- src/reasoned/ch1.clj
+++ src/reasoned/ch1.clj
@@ -1,9 +1,13 @@
 (ns reasoned.ch1
   (:require
-   [clojure.core.logic :as logic :refer [run* == u# s# fresh conde]])
+   [clojure.core.logic :as logic :refer [run run* == u# s# fresh conde]])
   (:refer-clojure :exclude [==]))
 
+;; > What is the name of s#?
+;; < succeed, because it succeeds
 
+;; > What is u#?
+;; < fail, it is a goal that fails.
 
 (run* [q]
   u#)
@@ -24,31 +28,202 @@
   (== :corn q))
 
 (run* [q]
+  u#
+  (== :corn q))
+
+(run* [q]
+  s#
+  (== false q))
+
+(run* [q]
   (fresh [x]
     (== true x)
     (== true q)))
 
+;; > What does the value of x represent?
 (run* [x]
   s#)
+;; < This represents a fresh variable
 
+;; > When is a variable fresh?
+;; < When it has no association.
 
-;; The following is really interesting, as it demonstrates that the two occurences
-;; of _0 represent different variables.
+(run* [x]
+  (let [x false]
+    (fresh [x]
+      (== true x))))
+
+;; > What observation can be made comparing the results of the following functions?
+(run* [r]
+  (fresh [x]
+    (let [y x]
+      (fresh [x]
+        (== (cons y (cons x (cons y (list)))) r)))))
+
+(run* [r]
+  (fresh [x]
+    (let [y x]
+      (fresh [x]
+        (== (cons x (cons y (cons x (list)))) r)))))
+;; < The value associated is the same ((_0 _1 _0)), this is because r's values are reified
+;; < in the order in which they appear in the list.
+
+(run* [q]
+  (== false q)
+  (== true q))
+
+(run* [q]
+  (== false q)
+  (== false q))
+
+;; > Do x and q co-refer or share?
+(run* [q]
+  (let [x q]
+    (== true x)))
+;; < No, they are the same variable.
+
+;; > The law of fresh
+;; < If x is fresh, then (== v x) succeeds and associates x with v.
+
+;; > The law of ==
+;; < (== v w) is the same as (== w v)
+
+;; > We can say that x and r ____ or _____?
+(run* [r]
+  (fresh [x]
+    (== x r)))
+;; < co-refer or share
+
+(run* [q]
+  (fresh [x]
+    (== true x)
+    (== x q)))
+
+(run* [q]
+  (fresh [x]
+    (== x q)
+    (== true x)))
+
+;; > Are q and x different variables in
+(run* [q]
+  (fresh [x]
+    (== true x)
+    (== x q)))
+
+;; > What does this demonstrate?
+(run* [q]
+  (fresh [x]
+    (== (= x q) q)))
+;; < That x and q are different variables (not equal)
+
+;; > What does this demonstrate?
+(run* [q]
+  (let [x q]
+    (fresh [q]
+      (== (= x q) x))))
+;; < That every variable introduced by run or fresh is different from every other variable
+;; < introduced by fresh or run.
+
+(run* [x]
+  (conde
+   ((== :olive x) s#)
+   ((== :oil x) s#)
+   (s# u#)))
+
+;; > The law of conde
+;; < To get more values from conde, pretend that the successful conde line has failed,
+;; < refreshing all variables that got an association from that line.
+
+;; > What does the e stand for in conde?
+;; < It stands for every line, since every line can succeed.
+
+(run 1 [x]
+  (conde
+   ((== :olive x) s#)
+   ((== :oil x) s#)
+   (s# u#)))
+
+(run* [x]
+  (conde
+   ((== :virgin x) u#)
+   ((== :olive x) s#)
+   (s# s#)
+   ((== :oil x) s#)
+   (s# u#)))
+
+(run 2 [x]
+  (conde
+   ((== :extra x) s#)
+   ((== :virgin x) u#)
+   ((== :olive x) s#)
+   ((== :oil x) s#)
+   (s# u#)))
+
+(run 2 [r]
+  (fresh [x y]
+    (== :split x)
+    (== :pea y)
+    (== (list x y) r)))
+
+(run* [r]
+  (fresh [x y]
+    (conde
+     ((== :split x) (== :pea y))
+     ((== :navy x) (== :bean y)))
+    (== (list x y) r)))
+
+;; > Given
+(defn teacupo [x]
+  (conde
+   ((== :tea x) s#)
+   ((== :cup x) s#)))
+;; > What is the value of
+(run* [r]
+  (teacupo r))
+;; <
+
+;; > Given
+(defn teacupo [x]
+  (conde
+   ((== :tea x) s#)
+   ((== :cup x) s#)))
+;; > What is the value of
+(run* [r]
+  (fresh [x y]
+    (conde
+     ((teacupo x) (== true y) s#)
+     ((== false x) (== true y)))
+    (== (list x y) r)))
+;; <
+
 (run* [r]
   (fresh [x y z]
     (conde
-     [(== y x) (fresh [x] (== z x))]
-     [(fresh [x] (== y x)) (== z x)]
-     [s# u#])
+     ((== x y) (fresh [x] (== z x)))
+     ((fresh [x] (== y x)) (== z x)))
+    (== (list y z) r)))
+
+(run* [r]
+  (fresh [x y z]
+    (conde
+     ((== x y) (fresh [x] (== z x)))
+     ((fresh [x] (== y x)) (== z x)))
     (== false x)
-    (== (cons y (cons z '())) r)))
+    (== (list y z) r)))
 
-;; > The law of fresh
-;; > If x is fresh, then (== v x) succeeds and associates x with v.
+;; > What does this demonstrate about a and b?
+(run* [q]
+  (let [a (== true q)
+        b (== false q)]
+    b))
+;; < It demonstrates that a and b are expressions, whose value is a goal.
 
-;; > The law of ==
-;; > (== v w) is the same as (== w v)
-
-;; > The law of conde
-;; > To get more values from conde, pretned that the successful conde line has failed,
-;; > refreshing all variables that got an association from that line.
+(run* [q]
+  (let [a (== true q)
+        b (fresh [x]
+            (== x q)
+            (== false x))
+        c (conde
+           ((== true q) s#)
+           (s# (== false q)))]
+    b))
