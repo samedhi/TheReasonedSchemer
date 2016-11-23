ce59ac065694bb717b13a372580f34145d173560
Stephen Cagle <stephen@read-line.com>
2016-11-23 07:48:49 -0800

    Parser splits into code and text parts

--- anki.py
+++ anki.py
@@ -7,19 +7,28 @@ src_dir = 'src/reasoned'
 
 filenames = os.listdir(src_dir)
 
+def parser(segments):
+    xs = []
+    for is_comment, group in itertools.groupby(segments, lambda s: s.startswith(';;')):
+        if is_comment:
+            gs = map(lambda s: s[3:].strip(), group) # take ';;<' and ';;>' off, remove extra spaces
+            s = ' '.join(gs) # combine multiline comments into one string
+            xs.append({'text': s})
+        else:
+            xs.append({'code': ''.join(group)})
+    return xs
+
 def process_segment(segment):
     try:
-        i = (i for i,s in enumerate(segment) if s.startswith(';; <')).next()
-        question = segment[0:i]
-        answer = segment[i:]
+        i = (i for i,s in enumerate(segment) if s.startswith(';;<')).next()
+        return {'question': parser(segment[0:i]), 'answer': parser(segment[i:])}
     except StopIteration:
-        question = segment
-        answer = segment
-    return {'question': question, 'answer': answer}
+        return {'expression': parser(segment)}
 
 for filename in filenames[0:1]:
     with open(os.path.join(src_dir, filename), 'r') as f:
         segments = []
+        # This for loop splits code into segments, dividing using 1 or more blank lines
         for is_empty, group in itertools.groupby(f.readlines(), lambda s: len(s.strip()) == 0):
             if not is_empty:
                 segments.append(list(group))
