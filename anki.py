65f257e4dc5a95f9275473c595f381a8b6ed4aa5
Stephen Cagle <stephen@read-line.com>
2016-11-27 11:54:56 -0800

    Get ch1 to fully compile with Anki

--- anki.py
+++ anki.py
@@ -12,62 +12,79 @@ def parser(segments):
     for is_comment, group in itertools.groupby(segments, lambda s: s.startswith(';;')):
         if is_comment:
             gs = map(lambda s: s[3:].strip(), group) # take ';;<' and ';;>' off, remove extra spaces
-            s = ' '.join(gs) # combine multiline comments into one string
-            xs.append({'text': s})
+            s = ' '.join(gs) + '\n' # combine multiline comments into one string
+            xs.append({'type': 'text', 'value': s})
         else:
-            xs.append({'code': ''.join(group)})
+            xs.append({'type': 'code', 'value': ' '.join(group)})
     return xs
 
 def process_segment(segment):
     try:
+        # The following throws StopIteration if no line contains a answer (';;<')
         i = (i for i,s in enumerate(segment) if s.startswith(';;<')).next()
         return {'question': parser(segment[0:i]), 'answer': parser(segment[i:])}
     except StopIteration:
-        return {'expression': parser(segment)}
+        return {'question': parser(segment)}
+
+def eval_segment(segment, accumulator):
+    for k,vs in segment.items():
+        assert k in ['question','answer']
+        for section in vs:
+            assert section['type'] in ['code', 'text']
+            if 'code' in section['type']:
+                accumulator.append(section['value'])
+                s = ' '.join(accumulator)
+                CMD = ['boot', 'load-code', '-c', '%s' % s]
+                try:
+                    section['result'] = subprocess.check_output(CMD)
+                except subprocess.CalledProcessError:
+                    print "===== CODE ======"
+                    print s
+                    print "===== CMD ======="
+                    print ' '.join(CMD)
+                    raise
 
 for filename in filenames[0:1]:
     with open(os.path.join(src_dir, filename), 'r') as f:
         segments = []
         accumulator = []
-        # This for loop splits code into segments, dividing using 1 or more blank lines
+        cards = ['\t']
+
+        # This for loop splits code into segments, dividing segments by 1 or more blank lines
         for is_empty, group in itertools.groupby(f.readlines(), lambda s: len(s.strip()) == 0):
             if not is_empty:
                 segments.append(list(group))
 
-        ns_declaration = segments.pop(0) # pop the ns declaration
-        accumulator.append(''.join(ns_declaration)) # add ns declaration to begining of accumulator
-        segments = map(process_segment, segments)
+        accumulator.append(''.join(segments.pop(0))) # add ns declaration to begining of accumulator
 
+        for i,segment in enumerate(segments):
+            s1 = process_segment(segment)
+            segments[i] = s1
+            eval_segment(s1, accumulator)
+
+        # give an answer to all the segments that end on a code block
+        for segment in segments:
+            if 'answer' not in segment:
+                v = segment['question'][-1].pop('result')
+                segment['answer'] = [{'type': 'text', 'value': v}]
+
+        # transforms all "code" into "text"
         for segment in segments:
             for k,vs in segment.items():
-                for section in vs:
-                    if 'code' in section:
-                        accumulator.append(section['code'])
-                        s = ' '.join(accumulator)
-                        CMD = ['boot', 'load-code', '-c', '%s' % s]
-                        try:
-                            section['result'] = subprocess.check_output(CMD)
-                        except subprocess.CalledProcessError:
-                            print "===== CODE ======"
-                            print evaled
-                            print "===== CMD ======="
-                            print ' '.join(CMD)
-                            raise
+                for i,v in enumerate(vs):
+                    if v['type'] == 'code':
+                        if 'result' in v:
+                            s = '%s=> %s' % (v['value'], v['result'])
+                        else:
+                            s = v['value']
+                        vs[i] = {'type': 'text', 'value': s}
 
-        cards = ['\t']
         for segment in segments:
-            if 'expression' in segment:
-                d = segment['expression'][0]
-                cards.append('%s\t%s' % (d['code'], d['result']))
-            else:
-                question = []
-                for d in segment['question']:
-                    if 'text' in d:
-                        question.append(d['text'])
-                    else:
-                        question.append(d['code'].strip())
-                        question.append(' => %s' % d['result'])
-                question = '\n'.join(question)
-                cards.append('%s\t%s' % (question, segment['answer'][0]['text']))
+            print segment
+            ks = ['question', 'answer']
+            q, a = [' '.join( map(lambda d: d['value'], segment[k]) ) for k in ks]
+            s = "%s <<>> %s" % (q, a)
+            s = s.replace('\n', '<br>') # Anki needs \n to specify csv
+            cards.append(s)
 
         print '\n'.join(cards)
